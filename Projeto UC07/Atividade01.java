7c207a36e837a376aefda0e2806ff5b6980106a5
Gabiru <gabrielacarvalhosz061@gmail.com>
2026-03-02 19:15:37 -0300

    Add files via upload

---
+++ Projeto UC07/Atividade01.java
@@ -0,0 +1,28 @@
+public class Atividade01 {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+
+    int y = 10;  
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
